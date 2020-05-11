5641bb137b0b42b69669d2093f0c610892d3f850
ryoua <1794970183@qq.com>
2020-05-11 17:06:14 +0800

    update

--- 283.移动零.java
+++ 283.移动零.java
@@ -7,7 +7,19 @@
 // @lc code=start
 class Solution {
     public void moveZeroes(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                exchange(nums, count, i);
+                count += 1;
+            } 
+        }
+    }
 
+    public void exchange(int[] nums, int count, int i) {
+        int x = nums[count];
+        nums[count] = nums[i];
+        nums[i] = x;
     }
 }
 // @lc code=end
