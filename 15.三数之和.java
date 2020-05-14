a1d2a03df127fa8e6517289f06ea6b39776349eb
ryoua <1794970183@qq.com>
2020-05-14 15:27:41 +0800

    update

---
+++ 15.三数之和.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=15 lang=java
+ *
+ * [15] 三数之和
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        return solution2(nums);
+    }
+
+    public List<List<Integer>> solution1(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if ((nums[i] + nums[j] + nums[k]) == 0) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        res.add(list);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    public List<List<Integer>> solution2(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for(int k = 0; k < nums.length - 2; k++){
+            if(nums[k] > 0) break;
+            if(k > 0 && nums[k] == nums[k - 1]) continue;
+            int i = k + 1, j = nums.length - 1;
+            while(i < j){
+                int sum = nums[k] + nums[i] + nums[j];
+                if(sum < 0){
+                    while(i < j && nums[i] == nums[++i]);
+                } else if (sum > 0) {
+                    while(i < j && nums[j] == nums[--j]);
+                } else {
+                    res.add(new ArrayList<Integer>(Arrays.asList(nums[k], nums[i], nums[j])));
+                    while(i < j && nums[i] == nums[++i]);
+                    while(i < j && nums[j] == nums[--j]);
+                }
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
--- 70.爬楼梯.java
+++ 70.爬楼梯.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * @lc app=leetcode.cn id=70 lang=java
  *
@@ -7,6 +10,11 @@
 // @lc code=start
 class Solution {
     public int climbStairs(int n) {
+        return solution4(n);
+    }
+
+    // 递归O(2^n), 很容易超时
+    public int solution1(int n) {
         if (n == 1)
             return 1;
         if (n == 2)
@@ -14,9 +22,57 @@ class Solution {
         return climbStairs(n - 1) + climbStairs(n - 2);
     }
 
-    public int solution1(int n) {
-        
+    // 递归 + 备忘录O(n) + O(n)
+    public int solution2(int n) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        if (n < 1)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        if (map.containsKey(n))
+            return map.get(n);
+        int sum = solution2(n - 1) + solution2(n - 2);
+        map.put(n, sum);
+        return sum;
+    }   
+
+    // DP,  O(n) O(n)
+    public int solution3(int n) {
+        if (n < 1)
+			return 0;
+		if (n == 1)
+			return 1;
+		if (n == 2)
+            return 2;
+        int sum[] = new int [n];
+        sum[0] = 1;
+        sum[1] = 2;
+        for (int i = 2; i < n; i++) {
+            sum[i] = sum[i - 1] + sum[i - 2];
+        }   
+        return sum[n - 1]; 
     }
+
+    // DP O(n), O(1)
+    public int solution4(int n) {
+		if (n < 1)
+			return 0;
+		if (n == 1)
+			return 1;
+		if (n == 2)
+			return 2;
+		int sum = 0, sum1, sum2;
+		sum1 = 1;
+		sum2 = 2;
+		for (int i = 2; i < n; i++) {
+			sum = sum1 + sum2;
+			sum1 = sum2;
+			sum2 = sum;
+		}
+		return sum;
+	}
 }
 // @lc code=end
 
